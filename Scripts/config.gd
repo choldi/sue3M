335539abcaac7b81050437c9f54ef17d7abdc251
choldi <jordias@gmail.com>
2024-07-26 20:26:45 +0200

    options and audio in progress

--- Scripts/config.gd
+++ Scripts/config.gd
@@ -9,6 +9,11 @@ var y_offset:int
 var start_minutes:int
 var start_seconds:int
 var score_file:String
+var vol_mixer:float
+var vol_music:float
+var vol_sfx:float
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,7 +26,14 @@ func _ready():
 	start_minutes=3
 	start_seconds=0
 	score_file="user://scoresave.cfg"
+
+	load_hiscore()
+	set_vol("Master",1)
+	set_vol("Music",1)
+	set_vol("SFX",1)	
 	pass # Replace with function body.
+
+
 
 func get_boardsize():
 	return Vector2(width,height)
@@ -42,4 +54,21 @@ func load_hiscore():
 	else:
 		save_hiscore("000000")
 	return content
+
+func set_vol(audio_src,vol):
+	if audio_src=="Master":
+		self.vol_mixer=vol
+	elif audio_src=="Music":
+		self.vol_music=vol
+	elif audio_src=="SFX":
+		self.vol_sfx=vol
+
+func get_vol(audio_src):
+	if audio_src=="Master":
+		return self.vol_mixer
+	elif audio_src=="Music":
+		return self.vol_music
+	elif audio_src=="SFX":
+		return self.vol_sfx
+
 
--- Scripts/volSlider.gd
+++ Scripts/volSlider.gd
@@ -7,6 +7,7 @@ func _ready():
 	bus_index=AudioServer.get_bus_index(bus_name)
 	value_changed.connect(_on_value_changed)
 	value=db_to_linear(AudioServer.get_bus_volume_db(bus_index))
+	GlobalVars.set_vol(bus_name,value)
 	pass # Replace with function body.
 
 func _on_value_changed(value:float):
